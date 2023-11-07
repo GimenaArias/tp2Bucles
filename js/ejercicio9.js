7338e500a462adfd52fd5db6a5134cbc7443a2fe
GimenaArias <gime.arias.az@gmail.com>
2023-11-07 15:14:50 -0300

    agregue el ejercicio 9

---
+++ js/ejercicio9.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 500; i++) {
+    if (i % 4 === 0 && i % 9 === 0) {
+      document.write(i + "-/Múltiplo de 4 y 9<br>");
+    } else if (i % 4 === 0) {
+      document.write(i + "-/Múltiplo de 4<br>");
+    } else if (i % 9 === 0) {
+      document.write(i + "-/Múltiplo de 9<br>");
+    } else {
+      document.write(i + "<br>");
+    }
+    if (i % 5 === 0) {
+      document.write("————————————————————<br>");
+    }
+  }
